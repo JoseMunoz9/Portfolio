be5baea9203335c61610c19884d19245087d09db
Jose Munoz <jommunoz90@gmail.com>
2022-06-13 20:20:39 +0100

    Second commit

--- LoginSystemGUI/src/LoginSystem.java
+++ LoginSystemGUI/src/LoginSystem.java
@@ -1,9 +1,73 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class LoginSystem {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+public class LoginSystem implements ActionListener{
+	
+	private static JLabel userLabel;
+	private static JTextField userText;
+	private static JLabel passLabel;
+	private static JPasswordField passText;
+	private static JButton button;
+	private static JLabel success;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		JPanel panel = new JPanel();
+		JFrame frame = new JFrame();
+		frame.setSize(350, 200);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		frame.add(panel);
+		
+		panel.setLayout(null);
+		
+		userLabel = new JLabel("User");
+		userLabel.setBounds(10, 20, 80, 25);
+		panel.add(userLabel);
+		
+		userText = new JTextField(20);
+		userText.setBounds(100, 20, 165, 25);
+		panel.add(userText);
+		
+		passLabel = new JLabel("Password");
+		passLabel.setBounds(10, 50, 80, 25);
+		panel.add(passLabel);
+		
+		passText = new JPasswordField(20);
+		passText.setBounds(100, 50, 165, 25);
+		panel.add(passText);
+		
+		button = new JButton("Login");
+		button.setBounds(10, 80, 80, 25);
+		button.addActionListener(new LoginSystem());
+		panel.add(button);
+		
+		success = new JLabel("");
+		success.setBounds(10, 110, 300, 25);
+		panel.add(success);
 
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		String user = userText.getText();
+		String password = passText.getText();
+		
+		if(user.equals("Jose") && password.equals("covid19")) {
+			success.setText("Login successful!");
+		}
+		else {
+			success.setText("Incorrect user and/or password.");
+			
+		}
+		
 	}
 
 }
