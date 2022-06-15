b4152fc3c20d78472f49cf22ac5ce236c5ee4589
Jose Munoz <jommunoz90@gmail.com>
2022-06-15 11:58:23 +0100

    Developer's Signature

--- CalculatorProject/src/calculator.java
+++ CalculatorProject/src/calculator.java
@@ -1,5 +1,8 @@
 import java.util.Scanner;
 
+//@Author Jose Munoz https://www.linkedin.com/in/jose-munoz90/
+//@Author Jose Munoz https://github.com/JoseMunoz9
+
 public class calculator {
 
 	public static void main(String[] args) {
