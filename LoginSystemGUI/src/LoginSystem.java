3860560df54943b7af25fa95123d91d534e813fa
Jose Munoz <jommunoz90@gmail.com>
2022-06-15 11:56:28 +0100

    Developer's Signature

--- LoginSystemGUI/src/LoginSystem.java
+++ LoginSystemGUI/src/LoginSystem.java
@@ -8,6 +8,9 @@ import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
+//@Author Jose Munoz https://www.linkedin.com/in/jose-munoz90/
+//@Author Jose Munoz https://github.com/JoseMunoz9
+
 public class LoginSystem implements ActionListener{
 	
 	private static JLabel userLabel;
