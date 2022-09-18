11d831332012d81e8e458f2d3ad5f4ca576e4258
Jose Munoz <jommunoz90@gmail.com>
2022-09-18 15:03:05 +0100

    Third commit

--- BMICalculator/src/BMICalculator.java
+++ BMICalculator/src/BMICalculator.java
@@ -7,19 +7,36 @@ import java.util.Scanner;
 public class BMICalculator {
 	
 	// Weight (kg) / Height (in) ^2 * 703
+	
+	private String name;
+	private int weight;
+	private int height;
+	private double bmi;
 
 	public static void main(String[] args) {
+		new BMICalculator();
+	}
+	
+	BMICalculator(){
 		Scanner userInput = new Scanner(System.in);
+		
+		System.out.println("Hello and welcome to BMI calculator. Please, enter your name:");
+		Scanner myName = new Scanner(System.in);
+		name = myName.nextLine();
 		
 		// Asking the user to type in their weight
 		
-		System.out.println("Please, enter you weight in Pounds (lb): ");
-			double weight = userInput.nextDouble();
+		System.out.println("Thanks " + name + ". Let us calculate your BMI.");
+		System.out.println("Enter you weight in Pounds (lb): ");
+			Scanner myWeight = new Scanner(System.in);
+			int weight = myWeight.nextInt();
 		
 		// Asking the user to enter their height
 		
-		System.out.println("Now, enter your height in Inches (in): ");
-		double height = userInput.nextDouble();
+		System.out.println("Great, now enter your height in Inches (in): ");
+			Scanner myHeight = new Scanner(System.in);
+			int height = userInput.nextInt();
+		
 		System.out.println("You have confirmed your weight AND height being " + weight + " - " + height);
 		
 		
@@ -27,8 +44,8 @@ public class BMICalculator {
 		
 		double bmi = weight / Math.pow(height, 2) * 703;
 		
-		DecimalFormat df = new DecimalFormat("0.0"); // With this line of code we are using the first decimal only
-		System.out.println("Your BMI is: " + df.format(bmi));
+		DecimalFormat df = new DecimalFormat("0.0"); // With this line of code we are using the first decimal place
+		System.out.println(name + ", your BMI is: " + df.format(bmi));
 		
 		userInput.close();
 
