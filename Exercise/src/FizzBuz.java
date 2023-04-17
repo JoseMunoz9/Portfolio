e5743704834c750e42b621fd226a8db0fa00ae8c
Jose Munoz <jommunoz90@gmail.com>
2023-04-17 11:04:15 +0100

    Updates

---
+++ Exercise/src/FizzBuz.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+// @Author Jose Munoz https://www.linkedin.com/in/jose-munoz90/
+// @Author Jose Munoz https://github.com/JoseMunoz9
+
+// Given a number 'n', for each integer 'i' in the range from 1 to 'n' inclusive,
+// print one value per line as follows:
+// If 'i' is a multiple of  both 3 and 5, print: FizzBuzz
+// If 'i' is a multiple of 3 (but not 5), print Fizz
+// If 'i' is a multiple of 5 (but not 3), print Buzz
+// If 'i' is not multiple of 3 or 5, print the value of 'i'
+public class FizzBuz {
+
+	public static int fizzBuzz(int n) {
+		// Declaring integer 'j' with the value of 15
+		int j = 15;
+		
+		// FOR loop that iterates from 1 to 'j' (15)
+		for(int i = 1; i <= j; i++) {
+			
+			// IF statement: If 'i' is a multiple of both 3 AND 5. In other words, if 'i'
+			// is divisible by 15.
+			if(i % 15 == 0) {
+				System.out.println("FizzBuzz");
+			} 
+			// IF 'i' is not multiple of 15, we check if it is a multiple of 3
+			else if (i % 3 == 0) {
+				System.out.println("Fizz");
+			} 
+			// IF 'i' is not multiple of 15 or 3, we check if it is a multiple of 5
+			else if (i % 5 == 0) {
+				System.out.println("Buzz");
+			}
+			// If 'i' is not a multiple of 15, 3 or 5, we execute the following code block:
+			else {
+				System.out.println(i);
+			}
+		}
+		return j;
+	}
+	public static void main(String[] args) {
+		Scanner myFizzBuzz = new Scanner(System.in);
+		
+		int i;
+		System.out.println("=====================================");
+		System.out.println(" ** WELCOME TO THE FIZZ-BUZZ GAME ** ");
+		System.out.println("=====================================");
+		
+		System.out.println("\nEnter an integer");
+		i = myFizzBuzz.nextInt();
+		
+		System.out.println(fizzBuzz(i));
+		
+		myFizzBuzz.close();
+	}
+
+}
--- Exercise/src/SumOfTwo.java
+++ Exercise/src/SumOfTwo.java
@@ -1,9 +1,10 @@
+import java.util.Scanner;
 // Given a two-digit integer 'n', return the sum of its digits.
 
 
 public class SumOfTwo {
 
-	int solution(int n) {
+	public static int solution(int n) {
 		// Declaring the variables:
 		String result = String.valueOf(n);
 		int[] digits = new int [result.length()];
@@ -15,6 +16,30 @@ public class SumOfTwo {
 		
 		// Here we return the result for the addition of the 2 digit integer.
 		return digits[0] + digits[1];
+	}
+	
+	public static void main(String[] args) {
+		int n;
+		Scanner mySum = new Scanner(System.in);
+		
+		System.out.println("===============================================");
+		System.out.println(" ** WELCOME TO THE SUM OF TWO-DIGITS INTEGER **");
+		System.out.println("===============================================");
+		
+		System.out.println("Here...");
+		System.out.println("\t> You will enter a TWO-DIGITS integer (N)");
+		System.out.println("\t> We will return the sum its digits.");
+		System.out.println();
+		System.out.println("Ready? Good, let's start..");
+		System.out.println("\t> Enter a TWO-DIGITS integer:");
+		n = mySum.nextInt();
+		
+		System.out.println("The sum of " + n + " is: " + solution(n));
+		
+		mySum.close();
+		System.out.println("===========================================");
+		System.out.println(" ** THANKS FOR PLAYING - HAVE A GOOD DAY **");
+		System.out.println("===========================================");
 	}
 
 }
